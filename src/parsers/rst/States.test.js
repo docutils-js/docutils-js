8c2566d4b28ae3b12cf1b270439ff8c68edad564
Kay <kay@kaymccormick.com>
2019-04-02 14:11:06 +0000

    testfixes

--- src/parsers/rst/States.test.js
+++
@@ -1,9 +0,0 @@
-import { RSTStateMachine } from './States';
-
-test('construct', () => {
-    let inputLines, document, inputOffset, matchTitles, inliner;
-    const rstSm = new RSTStateMachine({ inputLines, document, inputOffset,
-					matchTitles, inliner });
-    
-
-});
