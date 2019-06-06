8495bbca9da03126d3f2c6d1adb394c94b5e2787
Kay McCormick <kay@kaymccormick.com>
2019-06-06 16:21:38 +0000

    remove basesettings which is incompatible

--- src/parse.ts
+++ src/parse.ts
@@ -1,12 +1,9 @@
 import * as standalone from './fn/standaloneReader';
 import newDocument from './newDocument';
-import baseSettings from './baseSettings';
 import restParse from './fn/restructuredText';
+import {Settings} from "../gen/Settings";
 
-function parse(docSource, settings) {
-    if (typeof settings === 'undefined') {
-        settings = { ...baseSettings };
-    }
+function parse(docSource: string, settings?: Settings) {
     const document = newDocument({ sourcePath: '' }, settings);
     if (!document.reporter) {
         throw new Error('need document reporter');
