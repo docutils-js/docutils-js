8a15db183d77e94c4e06ce3317fa5755013ef9dd
Kay McCormick <kay@kaymccormick.com>
2019-06-16 19:08:44 +0000

    updates

--- __tests__/plugin.test.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { handlers } from '../plugins/restructuredtext';
-
-test.skip('', () => {
-    const doclet = { description: 'test' };
-    handlers.newDoclet({ doclet });
-    expect(doclet.description).toBeDefined();
-});
