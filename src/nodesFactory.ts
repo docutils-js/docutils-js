04de4f72df9fb649144b317f90793c7fa85d2598
Kay McCormick <kay@kaymccormick.com>
2019-07-21 01:39:49 +0000

    initial revision

---
+++ src/nodesFactory.ts
@@ -0,0 +1,616 @@
+import * as nodes from './nodes';
+import { NodeInterface,Attributes } from './types';
+
+export default {
+    math_block: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.math_block(rawsource, text, children, attributes),
+
+    comment: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.comment(rawsource, text, children, attributes),
+
+    literal_block: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.literal_block(rawsource, text, children, attributes),
+
+    raw: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.raw(rawsource, text, children, attributes),
+
+    address: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.address(rawsource, text, children, attributes),
+
+    doctest_block: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.doctest_block(rawsource, text, children, attributes),
+
+    paragraph: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.paragraph(rawsource, text, children, attributes),
+
+    acronym: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.acronym(rawsource, text, children, attributes),
+
+    revision: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.revision(rawsource, text, children, attributes),
+
+    target: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.target(rawsource, text, children, attributes),
+
+    strong: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.strong(rawsource, text, children, attributes),
+
+    organization: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.organization(rawsource, text, children, attributes),
+
+    term: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.term(rawsource, text, children, attributes),
+
+    abbreviation: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.abbreviation(rawsource, text, children, attributes),
+
+    label: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.label(rawsource, text, children, attributes),
+
+    footnote_reference: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.footnote_reference(rawsource, text, children, attributes),
+
+    title: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.title(rawsource, text, children, attributes),
+
+    contact: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.contact(rawsource, text, children, attributes),
+
+    substitution_reference: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.substitution_reference(rawsource, text, children, attributes),
+
+    copyright: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.copyright(rawsource, text, children, attributes),
+
+    rubric: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.rubric(rawsource, text, children, attributes),
+
+    citation_reference: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.citation_reference(rawsource, text, children, attributes),
+
+    subscript: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.subscript(rawsource, text, children, attributes),
+
+    attribution: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.attribution(rawsource, text, children, attributes),
+
+    classifier: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.classifier(rawsource, text, children, attributes),
+
+    generated: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.generated(rawsource, text, children, attributes),
+
+    status: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.status(rawsource, text, children, attributes),
+
+    reference: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.reference(rawsource, text, children, attributes),
+
+    date: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.date(rawsource, text, children, attributes),
+
+    line: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.line(rawsource, text, children, attributes),
+
+    option_argument: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.option_argument(rawsource, text, children, attributes),
+
+    field_name: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.field_name(rawsource, text, children, attributes),
+
+    subtitle: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.subtitle(rawsource, text, children, attributes),
+
+    emphasis: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.emphasis(rawsource, text, children, attributes),
+
+    substitution_definition: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.substitution_definition(rawsource, text, children, attributes),
+
+    math: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.math(rawsource, text, children, attributes),
+
+    problematic: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.problematic(rawsource, text, children, attributes),
+
+    version: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.version(rawsource, text, children, attributes),
+
+    title_reference: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.title_reference(rawsource, text, children, attributes),
+
+    author: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.author(rawsource, text, children, attributes),
+
+    superscript: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.superscript(rawsource, text, children, attributes),
+
+    caption: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.caption(rawsource, text, children, attributes),
+
+    option_string: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.option_string(rawsource, text, children, attributes),
+
+    literal: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.literal(rawsource, text, children, attributes),
+
+    inline: (
+        rawsource?: string,
+        text?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.inline(rawsource, text, children, attributes),
+
+    list_item: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.list_item(rawsource, children, attributes),
+
+    option_list_item: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.option_list_item(rawsource, children, attributes),
+
+    colspec: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.colspec(rawsource, children, attributes),
+
+    option_list: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.option_list(rawsource, children, attributes),
+
+    hint: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.hint(rawsource, children, attributes),
+
+    tip: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.tip(rawsource, children, attributes),
+
+    table: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.table(rawsource, children, attributes),
+
+    note: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.note(rawsource, children, attributes),
+
+    caution: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.caution(rawsource, children, attributes),
+
+    admonition: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.admonition(rawsource, children, attributes),
+
+    figure: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.figure(rawsource, children, attributes),
+
+    footnote: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.footnote(rawsource, children, attributes),
+
+    topic: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.topic(rawsource, children, attributes),
+
+    bullet_list: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.bullet_list(rawsource, children, attributes),
+
+    compound: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.compound(rawsource, children, attributes),
+
+    footer: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.footer(rawsource, children, attributes),
+
+    tbody: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.tbody(rawsource, children, attributes),
+
+    thead: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.thead(rawsource, children, attributes),
+
+    field_list: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.field_list(rawsource, children, attributes),
+
+    definition_list: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.definition_list(rawsource, children, attributes),
+
+    definition_list_item: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.definition_list_item(rawsource, children, attributes),
+
+    decoration: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.decoration(rawsource, children, attributes),
+
+    error: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.error(rawsource, children, attributes),
+
+    transition: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.transition(rawsource, children, attributes),
+
+    image: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.image(rawsource, children, attributes),
+
+    field: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.field(rawsource, children, attributes),
+
+    block_quote: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.block_quote(rawsource, children, attributes),
+
+    sidebar: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.sidebar(rawsource, children, attributes),
+
+    description: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.description(rawsource, children, attributes),
+
+    entry: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.entry(rawsource, children, attributes),
+
+    section: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.section(rawsource, children, attributes),
+
+    line_block: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.line_block(rawsource, children, attributes),
+
+    danger: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.danger(rawsource, children, attributes),
+
+    header: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.header(rawsource, children, attributes),
+
+
+    important: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.important(rawsource, children, attributes),
+
+    option: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.option(rawsource, children, attributes),
+
+    warning: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.warning(rawsource, children, attributes),
+
+    container: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.container(rawsource, children, attributes),
+
+    field_body: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.field_body(rawsource, children, attributes),
+
+    legend: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.legend(rawsource, children, attributes),
+
+    definition: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.definition(rawsource, children, attributes),
+
+    docinfo: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.docinfo(rawsource, children, attributes),
+
+    enumerated_list: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.enumerated_list(rawsource, children, attributes),
+
+    row: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.row(rawsource, children, attributes),
+
+    tgroup: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.tgroup(rawsource, children, attributes),
+
+    option_group: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.option_group(rawsource, children, attributes),
+
+    authors: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.authors(rawsource, children, attributes),
+
+    attention: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.attention(rawsource, children, attributes),
+
+    citation: (
+        rawsource?: string,
+        children: NodeInterface[] = [],
+        attributes: Attributes = {}
+    ) => new nodes.citation(rawsource, children, attributes),
+
+    Text: (data: string, rawsource?: string) => new nodes.Text(data, rawsource)
+};
